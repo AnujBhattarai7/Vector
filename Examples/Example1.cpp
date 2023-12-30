3e00dc729d465828615e19322efadf21fc0ba6f1
AnujBhattarai7 <war39413@gmail.com>
2023-12-30 12:25:43 +0545

    First commit

---
+++ Examples/Example1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Vector.h"
+
+int main(){
+   Vector<int> _V{1, 2};
+}
---
+++ Src/Vector.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define __PRINT__(x) {std::cout << "[VECTOR]: " << x << "\n";}
+
+#include <initializer_list>
+
+template<typename T>
+class Vector
+{
+private:
+public:
+    Vector(const std::initializer_list<T>& _V){
+        __PRINT__("Array")
+    }
+
+    ~Vector() {}
+};
