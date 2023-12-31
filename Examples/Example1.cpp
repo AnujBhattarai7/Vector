d31b373d61797a260d95de33d60f8af48233cfce
AnujBhattarai7 <war39413@gmail.com>
2023-12-31 08:29:04 +0545

    Third commit

--- Examples/Example1.cpp
+++ Examples/Example1.cpp
@@ -3,4 +3,7 @@
 
 int main(){
    Vector<int> _V{1, 2};
+   __PRINT__(_V.Flush())
+   __PRINT__(_V.Size())
+   __PRINT__(_V._Capacity())
 }
--- Src/Vector.h
+++ Src/Vector.h
@@ -4,16 +4,57 @@
 
 #include <initializer_list>
 
-template<typename T>
+template<typename _T>
 class Vector
 {
 private:
+    _T* _A = nullptr; // _A = Array
+    int _S = 0, _C = 0; // _S = Size of _A, _C = Total capacity of _A
+
 public:
-    Vector(const std::initializer_list<T>& _V){
-        __PRINT__("Array")
+    Vector(const std::initializer_list<_T>& _V);
+    ~Vector();
+
+    inline int Size() {return _S;}
+    inline int _Capacity() {return _C;}
+    inline bool Flush() {return _Flush();}
+private:
+    void _Alloc(int _S); // Allocates memory in the _A
+    bool _Flush(); // Flushes the Array(_A)
+    void _Reset(); // Resets The basic values of Vector
+};
+
+template<typename _T>
+Vector<_T>::Vector(const std::initializer_list<_T>& _V)
+{
+    
+}
+
+template<typename _T>
+Vector<_T>::~Vector()
+{
+    _Flush();
+}
+
+template<typename _T>
+bool Vector<_T>::_Flush()
+{
+    if (_A != nullptr)
+    {
+        delete[] _A;
+        _A = nullptr;
+        _Reset();
     }
 
-    ~Vector() {}
+    if (_A == nullptr)
+        return true;
 
-    void Set() { "asdas";}  
-};
+    return false;
+}
+
+template<typename _T>
+void Vector<_T>::_Reset()
+{
+    _S = 0;
+    _C = 0;
+}
